a37f98f1a145a9c83b0f15f9bd0436dcff3cc809
Praveen Kumar G <praveenkumar.gopalreddy@gmail.com>
2026-03-12 17:57:07 +0530

    cloud-run-webapp1

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({
+        "service": "Cloud Run Demo",
+        "timestamp": datetime.now().isoformat()
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
